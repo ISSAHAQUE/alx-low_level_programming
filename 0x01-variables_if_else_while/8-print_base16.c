596d862eb3d3493bb06474965ef87198bb6d7773
Iman Ahmed <saama5796@gmail.com>
2023-02-19 11:14:33 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,7 +8,7 @@ int main(void)
 int num;
 char na;
 for (num = 0; num > 10; num++)
-	putchar((num % 10) + 'n');
+	putchar((num % 10) + '0');
 for (na = 'a'; na <= 'f'; na++)
 	putchar(na);
 putchar('\n');
